440537741d4fa294652add77045d4ceb807d1c7e
Roman Elizarov <elizarov@devexperts.com>
2016-02-08 15:53:49 +0300

    Initial commit with todos

---
+++ tdp-core/src/main/java/com/devexperts/tools/tdp/TDPMain.java
@@ -0,0 +1,29 @@
+/*
+ * TDP - Thread Dump Parser
+ * Copyright (C) 2002-2016 Devexperts LLC
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ */
+package com.devexperts.tools.tdp;
+
+public class TDPMain {
+//    private final Option locks =
+//            new Option('l', "locks", "Use full original stacktraces when concurrent lock in contended state.");
+//    private final OptionInteger topdepth =
+//            new OptionInteger('t', "topdepth", "<number>", "Depth of reverse stack trace in tops section, by default 50.", 0, 1000, 50);
+//    private final OptionInteger methoddepth =
+//            new OptionInteger('m', "methoddepth", "<number>", "Depth of stack trace in methods section, by default 50.", 0, 1000, 50);
+//    private final OptionDouble threshold =
+//            new OptionDouble('h', "threshold", "<number>", "Threshold for contribution to expand, by default 5 (%).", 0, 100, 5);
+//    private final OptionString state =
+//            new OptionString('s', "state", "<id>", "Limit report to a specific state, all states are reported by default");
+//    private final OptionString output =
+//            new OptionString('o', "output", "<file>", "Output file, by default 'profile.txt'.");
+
+    public static void main(String[] args) {
+        // todo: parse options, print help if invoked without argument,
+        // todo: invoke ThreaadDumpParser
+    }
+}
